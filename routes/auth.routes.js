4b14591bdc550446d020fb4f6fc2fa2b9ccaecb8
Armen Mkrtchian <armenmkrtchian93@gmail.com>
2019-12-27 02:41:02 +0400

    added login and register logic and User model

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,20 +1,81 @@
 const {Router} = require('express')
+const bcrypt = require('bcryptjs')
+const {check, validationResult} = require('express-validator')
+const User = require('../models/User')
 const router = Router()
 
 
 // /api/auth/register
-router.post('register', async (req, res) => {
+router.post(
+    '/register',
+    [
+        check('email', 'Email address is incorrect').isEmail(),
+        check('password', 'Minimum length of password is 6 symbols').isLength({ min: 6 })
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req)
+
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'Incorrect email or password'
+                })
+            }
+            const {email, password} = req.body
+
+            const candidate = await User.findOne({ email })
+
+            if (candidate) {
+                return res.status(400).json({ message: 'The email address you have entered is already registered'})
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 12)
+            const user = new User({ email, password: hashedPassword })
+
+            await user.save()
+            res.status(201).json({ message: 'User created successfully' })
+
+        } catch {
+            res.status(500).jsin({ message: 'Something went wrong, please try again!' })
+        }
+})
+
+// /api/auth/login
+router.post(
+    '/login',
+    [
+        check('email', 'Enter the correct email').normalizeEmail().isEmail(),
+        check('password', 'Enter the password').exists()
+    ],
+    async (req, res) => {
     try {
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: 'Incorrect data in login process'
+            })
+        }
+
         const {email, password} = req.body
+
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            return res.status(400).json({ message: 'user is not found' })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Incorrect password, please try again' })
+        }
 
     } catch {
         res.status(500).jsin({ message: 'Something went wrong, please try again!' })
     }
-})
-
-// /api/auth/login
-router.post('/login', async (req, res) => {
-
-})
+})   
 
 module.exports = router
